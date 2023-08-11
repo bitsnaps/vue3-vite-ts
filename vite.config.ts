7a3de3c167d435056db44097da5fb3cbd1028def
Ibrahim.H <bitsnaps@yahoo.fr>
2023-08-11 11:01:40 +0100

    remove test config & untrack coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  test: {
-    // ...
-  }
 })
