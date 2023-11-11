c903e0a54e30c1542b19cd50d5727f767993e915
Ibrahim.H <bitsnaps@yahoo.fr>
2023-11-11 07:52:21 +0100

    configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
+/// <reference types="vitest" />
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +11,12 @@ export default defineConfig({
     alias: {
       '@': '/src'
     }
+  },
+  // We'll configure Vitest here (instead of vitest.config.ts file)
+  test: {
+    // Here we extend the default Vitest's default options
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    environment: 'jsdom',
+    root: fileURLToPath(new URL('./', import.meta.url))
   }
 })
