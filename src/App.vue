302f5e68bcf43b0c8b3b4492d508a46a6638cb86
Ibrahim.H <bitsnaps@yahoo.fr>
2023-11-11 12:26:47 +0100

    test: e2e posts

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,115 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+import { ref } from "vue";
+import axios from "axios";
 
+import PostCard from "@/components/PostCard.vue";
+import NotificationToast from "@/components/NotificationToast.vue";
+
+function getUserId(max = 10) {
+  return Math.floor(Math.random() * max);
+}
+
+const formData = ref({
+  title: "",
+  body: "",
+});
+const createdPost = ref(null);
+const loading = ref(false);
+const error = ref<string | null>(null);
+
+const handleSubmit = async (e: any) => {
+  e.preventDefault();
+
+  if (!formData.value.title) {
+    error.value = "Please input post title";
+    return;
+  }
+
+  if (!formData.value.body) {
+    error.value = "Please input post body";
+    return;
+  }
+
+  loading.value = true;
+  createdPost.value = null;
+  try {
+    const { data } = await axios.post(
+      "https://jsonplaceholder.typicode.com/posts",
+      {
+        userId: getUserId(),
+        title: formData.value.title,
+        body: formData.value.body,
+      }
+    );
+    createdPost.value = data;
+  } catch (err) {
+    if (axios.isAxiosError(err)){
+      error.value = err.message;
+    } else {
+      // This is the same error message sent by server
+      error.value = 'Error occurred';
+    }
+  } finally {
+    loading.value = false;
+  }
+};
+
+const clearErrorNotification = () => {
+  error.value = null;
+};
 </script>
 
-<template>
-  <div>
-    <p>
-      <!-- use the router-link component for navigation. -->
-      <!-- specify the link by passing the `to` prop. -->
-      <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
-        <br>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-    </p>
-    <!-- route outlet -->
-    <!-- component matched by the route will render here -->
-    <router-view></router-view>
-  </div>
 
+<template>
+  <div class="post-container">
+    <PostCard v-if="createdPost" :title="createdPost.title" :body="createdPost.body" />
+    <form v-else @submit="handleSubmit" data-testid="post-form" class="create-post-form">
+      <div class="post-title">
+        <label for="title">Title</label>
+        <input data-testid="title-input" type="text" name="title" v-model="formData.title" />
+      </div>
+      <div class="post-body">
+        <label for="body">Content</label>
+        <textarea data-testid="body-input" name="body" v-model="formData.body" rows="5" />
+      </div>
+      <button type="submit" :disabled="loading" class="create-post-btn">
+        {{ loading ? "Creating..." : "Create Post" }}
+      </button>
+    </form>
+    <NotificationToast v-if="error" status="error" :message="error" @clear-notification="clearErrorNotification" />
+  </div>
 </template>
 
+<style scoped>
+.post-container {
+  max-width: 560px;
+  margin: 5rem auto;
+}
+
+.create-post-form label {
+  color: #fff;
+}
+
+.create-post-form input,
+.create-post-form textarea {
+  margin-top: 8px;
+  padding: 12px;
+}
+
+.post-title {
+  display: flex;
+  flex-direction: column;
+}
+
+.post-body {
+  display: flex;
+  flex-direction: column;
+  margin-top: 14px;
+}
+
+.create-post-btn {
+  display: block;
+  margin: 16px auto;
+  cursor: pointer;
+}
+</style>
--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,47 +1,27 @@
 <script lang="ts" setup>
-
-import { ref, onMounted } from "vue";
-import axios /*, { AxiosError } */ from "axios";
-
-type Post = {
-    title: string,
-    body: string
-}
-
-const post = ref<Post>();
-const loading = ref(true);
-const error = ref<string | null | void>(null);
-
-const fetchPost = async () => {
-    try {
-        const { data } = await axios.get(
-            "https://jsonplaceholder.typicode.com/posts/1"
-        );
-        post.value = data;
-    } catch (err /*: AxiosError | unknown*/) {
-        if (axios.isAxiosError(err)) {
-            error.value = err.message;
-        } else {
-            error.value = 'Error occurred'
-        }
-    } finally {
-        loading.value = false;
-    }
-};
-
-onMounted(() => {
-    fetchPost();
+defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
 });
-
 </script>
 
 <template>
     <div>
-        <div v-if="post">
-            <h1 data-testid="post-title">{{ post.title }}</h1>
-            <p data-testid="post-body">{{ post.body }}</p>
-        </div>
-        <p v-if="loading" data-testid="loader">Loading...</p>
-        <p v-if="error" data-testid="error-message">{{ error }}</p>
+        <h1>{{ title }}</h1>
+        <p>{{ body }}</p>
     </div>
 </template>
+
+<style>
+h1 {
+    font-size: 32px;
+    font-weight: 700;
+    color: #fff;
+}
+</style>
